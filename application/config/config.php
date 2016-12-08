9a0f70d320949f93ab8331518677194b212acd4d
MGX82 <rifky@kudo.co.id>
2016-12-08 23:31:38 +0700

    edit config.php add  dinamic url

--- application/config/config.php
+++ application/config/config.php
@@ -16,7 +16,11 @@
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/sch';
+$root=(isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
+$root.= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+$config['base_url'] = $root;
+
+//$config['base_url'] = 'http://localhost/sch';
 /*
 |--------------------------------------------------------------------------
 | Index File
